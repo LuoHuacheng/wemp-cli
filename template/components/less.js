e9f2da89da8820dba1f7738307df30226068fa03
LuoHuacheng <154224099@qq.com>
2018-03-21 11:16:14 +0800

    修改less为stylus

--- template/components/less.js
+++
@@ -1,5 +0,0 @@
-module.exports = options => `/* ${options.dir}/${options.name}/${options.name}.less */
-.title {
-  font-size: 40rpx;
-}
-`;
---
+++ template/views/styl.js
@@ -0,0 +1,4 @@
+module.exports = options => `/* ${options.dir}/${options.name}/${options.name}.styl */
+.title
+  font-size: 20px;
+`;
